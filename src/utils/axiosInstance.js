b57052fc2875c8e6ed2a6c4f01e3a41eef289777
Nishita <nishitakarda4@gmail.com>
2025-07-07 00:16:12 +0530

    updated backend url

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -3,7 +3,7 @@ import { store } from '../redux/store';
 import { navigate } from './navigationHelper'; 
 
 const axiosInstance = axios.create({
-  baseURL: 'https://virtualgiving-backend.onrender.com/api',  // 'http://localhost:8080/api',
+  baseURL: 'https://virtualgiving-backend-zsz6.onrender.com/api',  // 'http://localhost:8080/api',
   withCredentials: true,
 });
 
